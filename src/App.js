bc840588b2e779c6449b09e92ee7862abedd56cf
Nicolas Médoc <nicolas.medoc@list.lu>
2024-11-11 16:50:13 +0100

    get Data from flask

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import ScatterplotContainer from "./components/scatterplot/ScatterplotContainer";
 import { useEffect} from 'react';
-import ControlBar from "./components/ControlBar/ControlBar";
+import {getProjectionData} from "./redux/DataSetSlice";
+import {useDispatch} from "react-redux";
+
 // here import other dependencies
 
 // a component is a piece of code which render a part of the user interface
 function App() {
+  const dispatch = useDispatch();
   useEffect(()=>{
     console.log("App useEffect");
   })
-
+  useEffect(()=>{
+    dispatch(getProjectionData());
+  },[]) // empty dependencies [] <=> component did mount
   return (
     <div className="App">
       {console.log("App rendering")}
--- src/redux/DataSetSlice.js
+++ src/redux/DataSetSlice.js
@@ -2,7 +2,12 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 // get the data in asyncThunk
 export const getProjectionData = createAsyncThunk('projectionData/fetchData', async (args,thunkAPI) => {
-    // when a result is returned, extraReducer below is triggered with the case getProjectionData.fulfilled
+  const response = await fetch('http://localhost:5000/getProjection');
+  const responseJson = await response.json();
+  return responseJson.projection.map((item,i)=>{
+    return {xValue:item[0], yValue:item[1], index:i, category:responseJson.categories[i]};
+  });
+  // when a result is returned, extraReducer below is triggered with the case getProjectionData.fulfilled
 })
 
 export const dataSetSlice = createSlice({
@@ -11,6 +16,14 @@ export const dataSetSlice = createSlice({
   reducers: {
       // add reducer if needed
   },
+  extraReducers: builder => {
+    builder
+        .addCase(getProjectionData.pending, (state, action)=>{})
+        .addCase(getProjectionData.fulfilled, (state, action) => {
+          return action.payload
+        })
+        .addCase(getProjectionData.rejected, (state, action)=>{})
+  }
 })
 
 // Action creators are generated for each case reducer function
